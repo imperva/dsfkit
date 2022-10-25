c07f1cef9a436efc7323b717dea3032d10b3ab9d
Eytan Naim <eytan.naim@imperva.com>
2022-10-25 16:42:20 +0300

    Update main.tf

--- deploy/modules/db_onboarding/main.tf
+++ deploy/modules/db_onboarding/main.tf
@@ -4,13 +4,16 @@ locals {
   db_username = "admin"
 }
 
-resource "random_password" "admin_password" {
+resource "random_password" "db_password" {
   length           = 15
   special          = false
 }
 
-resource "random_pet" "pet" {
+resource "random_pet" "db_name" {
   separator = "_"
+}
+
+resource "random_pet" "db_id" {
 }
 
 #################################
@@ -18,13 +21,13 @@ resource "random_pet" "pet" {
 #################################
 resource "aws_db_instance" "default" {
   allocated_storage    = 20
-  db_name              = "db_demo_${random_pet.pet.id}"
-  identifier           = "edsf-db-demo"
+  db_name              = "db_demo_${random_pet.db_name.id}"
+  identifier           = "edsf-db-demo-${random_pet.db_id.id}"
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   username             = local.db_username
-  password             = random_password.admin_password.result
+  password             = random_password.db_password.result
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   lifecycle {
@@ -41,7 +44,7 @@ data "template_file" "onboarder" {
     ssh_key_path        = var.hub_ssh_key_path
     assignee_gw         = var.assignee_gw
     db_user             = local.db_username
-    db_password         = random_password.admin_password.result
+    db_password         = random_password.db_password.result
     db_arn              = aws_db_instance.default.arn
     module_path         = path.module
   }
