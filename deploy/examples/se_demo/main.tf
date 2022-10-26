da89d2af585314381b5652c84cd754107be0558e
Eytan Naim <eytan.naim@imperva.com>
2022-10-26 22:25:38 +0300

    Example | Fix vpc for se demo
    Former-commit-id: 4d48b41f3de70407b56a7f678917415253ef0146

--- deploy/examples/se_demo/main.tf
+++ deploy/examples/se_demo/main.tf
@@ -73,6 +73,9 @@ module "vpc" {
 
   cidr = "10.0.0.0/16"
 
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
   azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
