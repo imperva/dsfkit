03ad0dbdb3efe55885ac7b79873dfdb6fa503d4d
Eytan Naim <eytan.naim@imperva.com>
2022-10-26 08:19:25 +0300

    se demo | Fix vpc args

--- deploy/examples/se_demo/main.tf
+++ deploy/examples/se_demo/main.tf
@@ -76,9 +76,6 @@ module "vpc" {
   azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = false
-  single_nat_gateway = true
 }
 
 ##############################
