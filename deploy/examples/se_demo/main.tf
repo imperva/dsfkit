a8c8f971a63b984b56f87f310e7bb9f229ea2c23
Eytan Naim <eytan.naim@imperva.com>
2022-10-31 19:21:04 +0200

    Fix vpc module in exmpales
    Former-commit-id: 016909e0fe00e03e7d367f569adb913c698643da

--- deploy/examples/se_demo/main.tf
+++ deploy/examples/se_demo/main.tf
@@ -89,8 +89,8 @@ module "vpc" {
   single_nat_gateway = true
 
   azs             = slice(data.aws_availability_zones.available.names, 0, 2)
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
   tags            = local.tags
 }
 
