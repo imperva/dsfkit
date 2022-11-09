371a2d829cc34e863959ade1c245047fb366f565
eytannnaim <eytan.naim@imperva.com>
2022-11-09 13:05:55 +0200

    Remove unneeded modules

--- deploy/modules/dsf_base_instance/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  description = "Public Elastic IP address of the DSF base instance"
-  value       = aws_eip.dsf_instance_eip.public_ip
-}
-
-output "private_ip" {
-  description = "Private IP address of the DSF base instance"
-  value       = aws_instance.dsf_base_instance.private_ip
-}
