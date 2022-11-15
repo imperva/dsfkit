971a06ba137c48ce584dbb2c8e93ac15c1850f4b
eytannnaim <eytan.naim@imperva.com>
2022-11-15 10:35:27 +0200

    Remove "sensitive" attribute from db password

--- deploy/modules/rds-mysql-db/output.tf
+++ deploy/modules/rds-mysql-db/output.tf
@@ -4,7 +4,7 @@ output db_username {
 }
 
 output db_password {
-  value = local.db_password
+  value = nonsensitive(local.db_password)
 }
 
 output db_name {
@@ -32,6 +32,6 @@ output "db_port" {
 }
 
 output "sql_cmd" {
-  value = "mysql -h${aws_db_instance.rds_db.address} --user ${local.db_username} mysql --password=${local.db_password}"
+  value = "mysql -h${aws_db_instance.rds_db.address} --user ${local.db_username} mysql --password=${nonsensitive(local.db_password)}"
 }
 
