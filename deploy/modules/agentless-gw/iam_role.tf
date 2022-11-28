b6fba4b00b943c4f3456e98305ef29796e53e16b
Eytan Naim <eytan.naim@imperva.com>
2022-11-28 17:44:35 +0200

    Fix last commit
    Former-commit-id: 38dbd2786965c66b93b7ba201f9243972040fabf

--- deploy/modules/agentless-gw/iam_role.tf
+++ deploy/modules/agentless-gw/iam_role.tf
@@ -18,6 +18,25 @@ locals {
       },
     ]
   })
+  inline_policy_s3 = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        "Resource" : [
+          "arn:aws:s3:::${var.installation_location.s3_bucket}",
+          "arn:aws:s3:::${var.installation_location.s3_bucket}/*",
+        ]
+      }
+    ]
+    }
+  )
+
 }
 
 resource "aws_iam_instance_profile" "dsf_gw_instance_iam_profile" {
@@ -30,6 +49,9 @@ resource "aws_iam_role" "dsf_gw_role" {
   name_prefix         = "imperva-dsf-gw-role"
   description         = "imperva-dsf-gw-role-${var.name}-${random_string.gw_id.result}"
   managed_policy_arns = null
-  assume_role_policy = local.role_assume_role_policy
+  assume_role_policy  = local.role_assume_role_policy
+  inline_policy {
+    name   = "imperva-dsf-s3-access"
+    policy = local.inline_policy_s3
+  }
 }
-
--- deploy/modules/hub/iam_role.tf
+++ deploy/modules/hub/iam_role.tf
@@ -18,6 +18,38 @@ locals {
       },
     ]
   })
+  inline_policy_secret = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : "secretsmanager:GetSecretValue",
+        "Resource" : [
+          "${local.secret_aws_arn}"
+        ]
+      }
+    ]
+    }
+  )
+  inline_policy_s3 = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        "Resource" : [
+          "arn:aws:s3:::${var.installation_location.s3_bucket}",
+          "arn:aws:s3:::${var.installation_location.s3_bucket}/*",
+        ]
+      }
+    ]
+    }
+  )
 }
 
 resource "aws_iam_instance_profile" "dsf_hub_instance_iam_profile" {
@@ -31,4 +63,12 @@ resource "aws_iam_role" "dsf_hub_role" {
   description         = "imperva-dsf-hub-role-${var.name}"
   managed_policy_arns = null
   assume_role_policy  = local.role_assume_role_policy
+  inline_policy {
+    name   = "imperva-dsf-s3-access"
+    policy = local.inline_policy_s3
+  }
+  inline_policy {
+    name   = "imperva-dsf-secret-access"
+    policy = local.inline_policy_secret
+  }
 }
