7911cf74119e6deb2f78eef8e140ff5d6026f451
Eytan Naim <eytan.naim@imperva.com>
2022-12-11 11:22:37 +0200

    Update outputs.tf
    Former-commit-id: 8798b99914bb2344b50cf689e6cb085fb41d9769

--- deploy/examples/multi_account/outputs.tf
+++ deploy/examples/multi_account/outputs.tf
@@ -6,7 +6,7 @@ output "dsf_agentless_gw_group" {
       jsonar_uid      = try(val.jsonar_uid, null)
       display_name    = try(val.display_name, null)
       role_arn        = try(val.iam_role, null)
-      ssh_command     = try("ssh -o ProxyCommand='ssh -o UserKnownHostsFile=/dev/null -i ${module.key_pair_hub.key_pair_private_pem.filename} -W %h:%p ec2-user@${module.hub.public_address}' -i ${module.key_pair_gw.key_pair_private_pem.filename} ec2-user@${val.private_address}", null)
+      ssh_command     = try("ssh -o ProxyCommand='ssh -o UserKnownHostsFile=/dev/null -i ${module.key_pair_hub.key_pair_private_pem.filename} -W %h:%p ec2-user@${module.hub.private_address}' -i ${module.key_pair_gw.key_pair_private_pem.filename} ec2-user@${val.private_address}", null)
     }
   }
 }
@@ -18,7 +18,7 @@ output "dsf_hubs" {
       jsonar_uid      = try(module.hub.jsonar_uid, null)
       display_name    = try(module.hub.display_name, null)
       role_arn        = try(module.hub.iam_role, null)
-      ssh_command     = try("ssh -i ${module.key_pair_hub.key_pair_private_pem.filename} ec2-user@${module.hub.public_address}", null)
+      ssh_command     = try("ssh -i ${module.key_pair_hub.key_pair_private_pem.filename} ec2-user@${module.hub.private_address}", null)
     }
   }
 }
