5c3a09becf36449a7361f655e1d7001d5827a088
Eytan Naim <eytan.naim@imperva.com>
2023-01-11 21:27:07 +0200

    Fix "No resource schema found for local_sensitive_file" error

--- modules/aws/core/key_pair/variables.tf
+++ modules/aws/core/key_pair/variables.tf
@@ -1,13 +1,3 @@
-#   source             = "terraform-aws-modules/key-pair/aws"
-#   key_name_prefix    = "imperva-dsf-"
-#   create_private_key = true
-# }
-
-# resource "local_sensitive_file" "dsf_ssh_key_file" {
-#   content         = module.key_pair.key_pair_private_pem
-#   file_permission = 400
-#   filename        = "ssh_keys/dsf_ssh_key-${terraform.workspace}"
-
 variable "key_name_prefix" {
   type = string
 }
