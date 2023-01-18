a121790374bff3d2a611062fb4edcaf45d33e8ce
Eytan Naim <eytan.naim@imperva.com>
2023-01-18 20:35:11 +0200

    parent 922cbc91064473b6d41fe8fe8edc7db4dc9ec5ff author Eytan Naim <eytan.naim@imperva.com> 1674066911 +0200 committer Eytan Naim <eytan.naim@imperva.com> 1674069145 +0200
    Update terraform_pre_release.yml
    
    Update terraform_pre_release.yml
    
    [skip actions]
    
    Update terraform_pre_release.yml
    
    [skip actions]
    
    Update terraform_pre_release.yml
    
    [skip actions]
    
    Update terraform_pre_release.yml
    
    [skip actions]
    
    Update terraform_pre_release.yml
    
    [skip actions]
    
    Automatic commit before release [release=1.3.4] | [skip actions]
    
    Fix release workflow
    
    [skip actions]

--- examples/poc/hadr_deployment/main.tf
+++ examples/poc/hadr_deployment/main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 }
 
 module "globals" {
-  source  = "imperva/dsf-globals/aws"
-  version = "1.3.4" # latest release tag
+  source        = "imperva/dsf-globals/aws"
+  version       = "1.3.4" # latest release tag
   sonar_version = var.sonar_version
 }
 
