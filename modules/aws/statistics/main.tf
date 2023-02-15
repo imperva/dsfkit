59b6956e803a0c40b37704ababd728aee5dadb43
linda.nasredin <linda.nasredin@imperva.com>
2023-02-15 17:39:36 +0200

    EDSF-128 Add prefix and day partitioning to statistics bucket

--- modules/aws/statistics/main.tf
+++ modules/aws/statistics/main.tf
@@ -18,7 +18,7 @@ data "aws_caller_identity" "current" {}
 # ################################
 locals {
   statistics_cmds = templatefile("${path.module}/statistics.tpl", {
-    statistics_bucket_name = "04274532-55f0-11ed-bdc3-0242ac120002"
+    statistics_bucket_path = "04274532-55f0-11ed-bdc3-0242ac120002/UsageData"
     ip                     = trimspace(data.http.workstation_public_ip.response_body)
     account_id             = data.aws_caller_identity.current.account_id
     user_id                = data.aws_caller_identity.current.user_id
