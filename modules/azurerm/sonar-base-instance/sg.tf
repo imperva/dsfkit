d3b7f666ee204642e773891b5a522f38fd03f425
Eytan Naim <eytan.naim@imperva.com>
2023-02-15 19:26:26 +0200

    disable all-in-and-out sg | wip

--- modules/azurerm/sonar-base-instance/sg.tf
+++ modules/azurerm/sonar-base-instance/sg.tf
@@ -1,29 +1,19 @@
 locals {
-  cidr_blocks   = concat(var.sg_ingress_cidr, var.create_and_attach_public_elastic_ip ? try(["${data.azurerm_public_ip.vm_public_ip[0].ip_address}/32"], []) : [])
-  ingress_ports = [22, 8080, 8443, 3030, 27117]
-
-  ingress_cidrs_map = { for cidr in local.cidr_blocks : cidr => cidr }
-  ingress_ports_map = { for port in local.ingress_ports : port => port }
+  create_security_group_count = var.security_group_id == null ? 1 : 0
+  cidr_blocks                 = var.sg_ingress_cidr
+  ingress_ports               = [22, 8080, 8443, 3030, 27117]
+  ingress_ports_map           = { for port in local.ingress_ports : port => port }
 }
 
 resource "azurerm_network_security_group" "dsf_base_sg" {
+  count               = local.create_security_group_count
   name                = join("-", [var.name, "sg"])
   location            = var.resource_group.location
   resource_group_name = var.resource_group.name
 }
 
-# tbd: https://learn.microsoft.com/en-us/azure/virtual-network/network-security-groups-overview#security-rules
-
-# # resource "aws_security_group_rule" "all_in" {
-# #   type        = "ingress"
-# #   from_port   = 0
-# #   to_port     = 0
-# #   protocol    = "-1"
-# #   cidr_blocks = ["0.0.0.0/0"]
-# #   security_group_id = aws_security_group.dsf_base_sg.id
-# # }
-
 resource "azurerm_network_security_rule" "sg_rule_all_out" {
+  count                       = local.create_security_group_count
   name                        = join("-", [var.name, "sg", "all", "out"])
   priority                    = 100
   direction                   = "Outbound"
@@ -34,82 +24,35 @@ resource "azurerm_network_security_rule" "sg_rule_all_out" {
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = var.resource_group.name
-  network_security_group_name = azurerm_network_security_group.dsf_base_sg.name
+  network_security_group_name = azurerm_network_security_group.dsf_base_sg[0].name
 }
 
-# tbd: remove
-resource "azurerm_network_security_rule" "sg_rule_all_in" {
-  name                        = join("-", [var.name, "sg", "all", "in"])
-  priority                    = 100
+resource "azurerm_network_security_rule" "sg_cidr_ingress" {
+  count                        = local.create_security_group_count
+  name                         = join("-", [var.name, "sg", "all", "in"])
+  priority                     = 100
+  direction                    = "Inbound"
+  access                       = "Allow"
+  protocol                     = "Tcp"
+  source_port_range            = "*"
+  destination_port_ranges      = local.ingress_ports
+  source_address_prefixes = local.cidr_blocks
+  destination_address_prefix = "*"
+  resource_group_name          = var.resource_group.name
+  network_security_group_name  = azurerm_network_security_group.dsf_base_sg[0].name
+}
+
+resource "azurerm_network_security_rule" "sg_webconsole_ingress" {
+  count                       = (length(var.web_console_cidr) != 0) && (local.create_security_group_count > 0) ? 1 : 0
+  name                        = join("-", [var.name, "sg", "webconsole", "in"])
+  priority                    = 101
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
+  destination_port_range      = 8443
+  source_address_prefixes       = var.web_console_cidr
+  destination_address_prefix = "*"
   resource_group_name         = var.resource_group.name
-  network_security_group_name = azurerm_network_security_group.dsf_base_sg.name
+  network_security_group_name = azurerm_network_security_group.dsf_base_sg[0].name
 }
-
-# resource "azurerm_network_security_rule" "sg_rule_cidr_in" {
-#   for_each                    = local.ingress_cidrs_map
-#   name                        = join("-", [var.name, "sg", "in", each.key])
-#   priority                    = 100 + each.key
-#   direction                   = "Inbound"
-#   access                      = "Allow"
-#   protocol                    = "Tcp"
-#   source_port_range           = "*"
-#   destination_port_range      = local.ingress_ports
-#   source_address_prefix       = "*"
-#   destination_address_prefix  = each.value
-#   resource_group_name         = var.resource_group.name
-#   network_security_group_name = azurerm_network_security_group.dsf_base_sg.name
-# }
-
-# resource "aws_security_group_rule" "sg_self" {
-#   for_each          = local.ingress_ports_map
-#   type              = "ingress"
-#   from_port         = each.value
-#   to_port           = each.value
-#   protocol          = "tcp"
-#   self              = true
-#   security_group_id = aws_security_group.dsf_base_sg.id
-# }
-
-# resource "aws_security_group_rule" "sonarrsyslog_self" {
-#   type              = "ingress"
-#   from_port         = 10800
-#   to_port           = 10899
-#   protocol          = "tcp"
-#   self              = true
-#   security_group_id = aws_security_group.dsf_base_sg.id
-# }
-
-# resource "aws_security_group_rule" "sg_ingress_self" {
-#   type              = "ingress"
-#   from_port         = 0
-#   to_port           = 65535
-#   protocol          = "tcp"
-#   self              = true
-#   security_group_id = aws_security_group.dsf_base_sg.id
-# }
-
-# resource "aws_security_group_rule" "sg_web_console_access" {
-#   count             = length(var.web_console_cidr) == 0 ? 0 : 1
-#   type              = "ingress"
-#   from_port         = 8443
-#   to_port           = 8443
-#   protocol          = "tcp"
-#   cidr_blocks       = var.web_console_cidr
-#   security_group_id = aws_security_group.dsf_base_sg.id
-# }
-
-# resource "aws_security_group_rule" "sg_allow_ssh_in_vpc" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-#   cidr_blocks       = [data.aws_vpc.selected.cidr_block]
-#   security_group_id = aws_security_group.dsf_base_sg.id
-# }
