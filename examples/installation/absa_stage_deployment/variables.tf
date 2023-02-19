20a6fb74584d4cf76c0d2c3912794dbb5c22071f
linda.nasredin <linda.nasredin@imperva.com>
2023-02-19 12:49:10 +0200

    Copy from poc/basic_deployment

---
+++ examples/installation/absa_stage_deployment/variables.tf
@@ -0,0 +1,107 @@
+variable "deployment_name" {
+  type    = string
+  default = "imperva-dsf"
+}
+
+variable "sonar_version" {
+  type    = string
+  default = "4.10"
+}
+
+variable "gw_count" {
+  type        = number
+  default     = 1
+  description = "Number of agentless gateways"
+  validation {
+    condition     = var.gw_count > 0
+    error_message = "The gw_count value must be greater than 0."
+  }
+}
+
+variable "web_console_admin_password" {
+  sensitive   = true
+  type        = string
+  default     = null # Random
+  description = "Admin password (Random generated if not set)"
+}
+
+variable "web_console_cidr" {
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+  description = "CIDR blocks allowing DSF hub web console access"
+}
+
+variable "database_cidr" {
+  type        = list(string)
+  default     = null # workstation ip
+  description = "CIDR blocks allowing dummy database access"
+}
+
+variable "workstation_cidr" {
+  type        = list(string)
+  default     = null # workstation ip
+  description = "CIDR blocks allowing hub ssh and debugging access"
+}
+
+variable "additional_install_parameters" {
+  default     = ""
+  description = "Additional params for installation tarball. More info in https://docs.imperva.com/bundle/v4.10-sonar-installation-and-setup-guide/page/80035.htm"
+}
+
+variable "vpc_ip_range" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "VPC cidr range"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  description = "VPC private subnet cidr range"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+  description = "VPC public subnet cidr range"
+}
+
+variable "hub_ebs_details" {
+  type = object({
+    disk_size        = number
+    provisioned_iops = number
+    throughput       = number
+  })
+  description = "DSF Hub compute instance volume attributes. More info in sizing doc - https://docs.imperva.com/bundle/v4.10-sonar-installation-and-setup-guide/page/78729.htm"
+  default = {
+    disk_size        = 250
+    provisioned_iops = 0
+    throughput       = 125
+  }
+}
+
+variable "gw_group_ebs_details" {
+  type = object({
+    disk_size        = number
+    provisioned_iops = number
+    throughput       = number
+  })
+  description = "DSF gw compute instance volume attributes. More info in sizing doc - https://docs.imperva.com/bundle/v4.10-sonar-installation-and-setup-guide/page/78729.htm"
+  default = {
+    disk_size        = 75
+    provisioned_iops = 0
+    throughput       = 125
+  }
+}
+
+variable "db_types_to_onboard" {
+  type        = list(string)
+  default     = ["RDS MySQL"]
+  description = "DB types to onboard, available types are - 'RDS MySQL', 'RDS MsSQL' with data"
+  validation {
+    condition = alltrue([
+      for db_type in var.db_types_to_onboard : contains(["RDS MySQL", "RDS MsSQL"], db_type)
+    ])
+    error_message = "Valid values should contain at least one of the following: 'RDS MySQL', 'RDS MsSQL'."
+  }
+}
