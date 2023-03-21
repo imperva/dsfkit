d5a5fcb177050fbe6fd9ffd99b2b73f0388e3b16
linda.nasredin <linda.nasredin@imperva.com>
2023-03-21 16:57:47 +0200

    EDSF-172 Public DNS of Hub in Hub & GW's machine file is wrong

--- examples/poc/hadr_deployment/main.tf
+++ examples/poc/hadr_deployment/main.tf
@@ -76,7 +76,7 @@ module "hub_primary" {
   }
   ingress_communication = {
     additional_web_console_access_cidr_list = var.web_console_cidr
-    full_access_cidr_list                   = concat(local.workstation_cidr, ["${module.hub_secondary.private_ip}/32"])
+    full_access_cidr_list                   = concat(local.workstation_cidr, ["${module.hub_secondary.private_ip}/32"], [var.private_subnets[0]])
   }
   use_public_ip = true
   depends_on = [
@@ -102,7 +102,7 @@ module "hub_secondary" {
   }
   ingress_communication = {
     additional_web_console_access_cidr_list = var.web_console_cidr
-    full_access_cidr_list                   = concat(local.workstation_cidr, ["${module.hub_primary.private_ip}/32"])
+    full_access_cidr_list                   = concat(local.workstation_cidr, ["${module.hub_primary.private_ip}/32"], [var.private_subnets[0]])
   }
   use_public_ip = true
   depends_on = [
