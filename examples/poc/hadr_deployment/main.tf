0c7d45e75c1a59213f614482e3a67b416fc9d812
Linda Nasredin <linda.nasredin@imperva.com>
2023-03-23 16:19:42 +0200

    Support BYO subnet in the DSF node private IP fix (#132)

--- examples/poc/hadr_deployment/main.tf
+++ examples/poc/hadr_deployment/main.tf
@@ -44,6 +44,10 @@ locals {
   db_subnets                 = var.subnet_ids != null ? var.subnet_ids.db_subnet_ids : module.vpc[0].public_subnets
 }
 
+data "aws_subnet" "gw_subnet" {
+  id = local.gw_subnet
+}
+
 ##############################
 # Generating network
 ##############################
@@ -82,7 +86,7 @@ module "hub_primary" {
   }
   ingress_communication = {
     additional_web_console_access_cidr_list = var.web_console_cidr
-    full_access_cidr_list                   = concat(local.workstation_cidr, ["${module.hub_secondary.private_ip}/32"], [var.private_subnets[0]])
+    full_access_cidr_list                   = concat(local.workstation_cidr, ["${module.hub_secondary.private_ip}/32"], [data.aws_subnet.gw_subnet.cidr_block])
   }
   use_public_ip = true
   depends_on = [
@@ -108,7 +112,7 @@ module "hub_secondary" {
   }
   ingress_communication = {
     additional_web_console_access_cidr_list = var.web_console_cidr
-    full_access_cidr_list                   = concat(local.workstation_cidr, ["${module.hub_primary.private_ip}/32"], [var.private_subnets[0]])
+    full_access_cidr_list                   = concat(local.workstation_cidr, ["${module.hub_primary.private_ip}/32"], [data.aws_subnet.gw_subnet.cidr_block])
   }
   use_public_ip = true
   depends_on = [
