870a4f5d8529569688e13f9e9f9fbc6b7a6e6122
linda.nasredin <linda.nasredin@imperva.com>
2023-03-30 10:56:43 +0300

    10 GWs + new tarball with duplicate Id fix

--- examples/poc/hadr_deployment/variables.tf
+++ examples/poc/hadr_deployment/variables.tf
@@ -11,7 +11,7 @@ variable "sonar_version" {
 
 variable "gw_count" {
   type        = number
-  default     = 4
+  default     = 10
   description = "Number of agentless gateways"
   validation {
     condition     = var.gw_count > 0
