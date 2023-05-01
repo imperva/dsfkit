1a21d1179cf58f0ee8bfa2ab2b992be20637deeb
eytannnaim <80354890+eytannnaim@users.noreply.github.com>
2023-05-01 19:52:09 +0300

    DAM | MX | Drop license encryption salt

--- modules/aws/mx/license.tf
+++ modules/aws/mx/license.tf
@@ -4,17 +4,13 @@ resource "random_password" "passphrase" {
   special = false
 }
 
-resource "random_id" "encryption_salt" {
-  byte_length = 8
-}
-
 data "local_sensitive_file" "license_file" {
   filename = var.license_file
 }
 
 locals {
   cmd = <<EOF
-  echo '{"cipher_text": "'$(echo '${data.local_sensitive_file.license_file.content}' | openssl aes-256-cbc -pass pass:${random_password.passphrase.result} -md md5 -S ${random_id.encryption_salt.hex} | base64 | tr -d "\n" )'"}'
+  echo '{"cipher_text": "'$(echo '${data.local_sensitive_file.license_file.content}' | openssl aes-256-cbc -pass pass:${random_password.passphrase.result} -md md5 | base64 | tr -d "\n" )'"}'
 EOF
 }
 
