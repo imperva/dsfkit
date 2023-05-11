c735c95edc22795e3886ebb4b4aba5c7dd260d6c
segev.elmalech <segev.elmalech@imperva.com>
2023-05-11 15:14:36 +0300

    Revert "Add cleaning and reduce gw to 1 - EDSF-156"
    This reverts commit 8cedf5907e346e5d0b41f5fcc6d8edef33f66a26.

--- examples/installation/multi_account_deployment/backend.tf
+++ examples/installation/multi_account_deployment/backend.tf
@@ -1,11 +1,9 @@
-terraform {
-  backend "s3" {
-    # backend "local" {
-    #        bucket         = "tf-state-dsfkit-github-tests"
-    bucket         = "terraform-state-bucket-dsfkit-github-tests"
-    key            = "states/terraform.tfstate"
-    dynamodb_table = "terraform-state-lock"
-    #        region         = "ap-southeast-2"
-    region         = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+##   Fill in your bucket details
+#    bucket  = "myBucket"
+#    key     = "terraform.tfstate"
+#    region  = "us-east-1"
+#    profile = "myProfile"
+#  }
+#}
