d1ea8b5faf21d1b9f1965ad738d5fb1f72385085
eytannnaim <80354890+eytannnaim@users.noreply.github.com>
2023-05-17 15:36:05 +0300

    Use tags in multi account example

--- examples/installation/multi_account_deployment/providers.tf
+++ examples/installation/multi_account_deployment/providers.tf
@@ -1,33 +1,21 @@
 provider "aws" {
-  default_tags {
-    tags = local.tags
-  }
   profile = var.aws_profile_hub
   region  = var.aws_region_hub_primary
 }
 
 provider "aws" {
-  default_tags {
-    tags = local.tags
-  }
   profile = var.aws_profile_hub
   region  = var.aws_region_hub_secondary
   alias   = "hub-secondary"
 }
 
 provider "aws" {
-  default_tags {
-    tags = local.tags
-  }
   profile = var.aws_profile_gw
   region  = var.aws_region_gw_primary
   alias   = "gw-primary"
 }
 
 provider "aws" {
-  default_tags {
-    tags = local.tags
-  }
   profile = var.aws_profile_gw
   region  = var.aws_region_gw_secondary
   alias   = "gw-secondary"
