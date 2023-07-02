5f5fcf7c5584782a7d463c3dc6be83c4ebe4d18f
Eytan Naim <eytan.naim@imperva.com>
2023-07-02 11:48:06 +0300

    wip

--- modules/aws/db-with-agent/main.tf
+++ modules/aws/db-with-agent/main.tf
@@ -32,6 +32,7 @@ resource "aws_instance" "agent" {
   iam_instance_profile = aws_iam_instance_profile.dsf_node_instance_iam_profile.id
   user_data            = local.user_data
   metadata_options {
+    http_endpoint = "enabled"
     http_tokens = "required"
   }
   tags                 = merge(var.tags, { Name = join("-", [var.friendly_name]) })
