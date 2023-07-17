1598399f72ef0549de5b1a9f640995713aac6524
linda.nasredin <linda.nasredin@imperva.com>
2023-07-17 15:38:58 +0300

    Updated description of tarball_location variable

--- examples/installation/sonar_multi_account_deployment/variables.tf
+++ examples/installation/sonar_multi_account_deployment/variables.tf
@@ -126,7 +126,7 @@ variable "tarball_location" {
     s3_region = string
     s3_key    = string
   })
-  description = "S3 bucket location of the DSF installation software"
+  description = "S3 bucket location of the DSF installation software. s3_key is the full path to the tarball file within the bucket, for example, 'prefix/jsonar-x.y.z.w.u.tar.gz'"
   default     = null
 }
 
