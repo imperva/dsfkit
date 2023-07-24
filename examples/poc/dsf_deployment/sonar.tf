8aa96a5ea76f7b98665962f37257b99db5469ac0
linda.nasredin <linda.nasredin@imperva.com>
2023-07-24 12:16:09 +0300

    DSF POC: Fix for hubs in private subnet case

--- examples/poc/dsf_deployment/sonar.tf
+++ examples/poc/dsf_deployment/sonar.tf
@@ -3,7 +3,9 @@ locals {
   tarball_location   = module.globals.tarball_location
   agentless_gw_count = var.enable_sonar ? var.agentless_gw_count : 0
 
-  hub_cidr_list          = compact([data.aws_subnet.hub.cidr_block, data.aws_subnet.hub_secondary.cidr_block, try(format("%s/32", module.hub[0].public_ip), null), try(format("%s/32", module.hub_secondary[0].public_ip), null)])
+  hub_primary_public_ip   = length(module.hub[0].public_ip) > 0 ? format("%s/32", module.hub[0].public_ip) : null
+  hub_secondary_public_ip = length(module.hub_secondary[0].public_ip) > 0 ? format("%s/32", module.hub_secondary[0].public_ip) : null
+  hub_cidr_list           = compact([data.aws_subnet.hub.cidr_block, data.aws_subnet.hub_secondary.cidr_block, local.hub_primary_public_ip, local.hub_secondary_public_ip])
   agentless_gw_cidr_list = [data.aws_subnet.agentless_gw.cidr_block, data.aws_subnet.agentless_gw_secondary.cidr_block]
 }
 
