4684af45e4ca4309d234d538440d941d7d3f8dc9
linda.nasredin <linda.nasredin@imperva.com>
2023-08-09 14:20:45 +0300

    Sonar upgrade POC - Reordered functions according to python convention

--- examples/upgrade/python_upgrader/main.py
+++ examples/upgrade/python_upgrader/main.py
@@ -7,20 +7,27 @@ import os
 import re
 from remote_executor import run_remote_script, run_remote_script_via_proxy
 
+# Helper functions
+
 
 def slp(duration):
     time.sleep(duration)
 
 
-# Get the absolute path of the currently executing script
-def get_current_directory():
-    return os.path.dirname(os.path.abspath(__file__))
+def str_to_bool(arg):
+    # Convert string "true" or "false" to Python boolean value
+    return arg.lower() == "true"
 
 
 # Get the absolute path to a script file in the same directory as this
 def get_script_file_path(script_file_name):
     script_dir = get_current_directory()
     return os.path.join(script_dir, script_file_name)
+
+
+# Get the absolute path of the currently executing script
+def get_current_directory():
+    return os.path.dirname(os.path.abspath(__file__))
 
 
 def read_file_contents(file_path):
@@ -42,6 +49,22 @@ def build_python_script_run_command(script_contents, args, python_location):
     return f"sudo {python_location} -c '{script_contents}' {args}"
 
 
+def print_gw(agentless_gws):
+    for item in agentless_gws:
+        # Extract values from the JSON
+        ip = item.get("ip")
+        ssh_user = item.get("ssh_user")
+        ssh_key = item.get("ssh_private_key_file_path")
+        proxy = item.get("proxy")
+
+        # Use the extracted values
+        print(f"IP: {ip}")
+        print(f"SSH User: {ssh_user}")
+        print(f"SSH Key: {ssh_key}")
+        print(f"Proxy: {proxy}")
+        print("---")
+
+
 def run_remote_script_maybe_with_proxy(agentless_gw, script_contents, script_run_command):
     if agentless_gw.get("proxy") is not None:
         script_output = run_remote_script_via_proxy(agentless_gw.get("ip"),
@@ -60,141 +83,7 @@ def run_remote_script_maybe_with_proxy(agentless_gw, script_contents, script_run
                                           script_run_command)
     return script_output
 
-
-def extract_python_location(script_output):
-    pattern = r'Python location: (\S+)'
-    match = re.search(pattern, script_output)
-
-    if match:
-        return match.group(1)
-    else:
-        raise Exception("Pattern 'Python location: ...' not found in 'Get python location' script output")
-
-
-def run_get_python_location_script(agentless_gw):
-    script_file_path = get_script_file_path("get_python_location.sh")
-    script_contents = read_file_contents(script_file_path)
-    script_run_command = build_bash_script_run_command(script_contents)
-
-    script_output = run_remote_script_maybe_with_proxy(agentless_gw, script_contents, script_run_command)
-
-    print(f"'Get python location' bash script output: {script_output}")
-    python_location = extract_python_location(script_output)
-    print(f"Python location in Agentless Gateway {agentless_gw.get('ip')} is {python_location}")
-    return python_location
-
-
-def extract_preflight_validations_result(script_output):
-    pattern = r'Preflight validations result: ({.+})'
-    match = re.search(pattern, script_output)
-
-    if match:
-        return match.group(1)
-    else:
-        raise Exception("Pattern 'Preflight validations result: ...' not found in 'Run preflight validations' "
-                        "script output")
-
-
-def run_preflight_validations_script(agentless_gw, target_version, python_location):
-    script_file_path = get_script_file_path("run_preflight_validations.py")
-    script_contents = read_file_contents(script_file_path)
-    script_run_command = build_python_script_run_command(script_contents, target_version, python_location)
-    # print(f"script_run_command: {script_run_command}")
-
-    script_output = run_remote_script_maybe_with_proxy(agentless_gw, script_contents, script_run_command)
-    print(f"'Run preflight validations' python script output: {script_output}")
-    preflight_validations_result = extract_preflight_validations_result(script_output)
-    print(f"Preflight validation results in Agentless Gateway {agentless_gw.get('ip')} are {preflight_validations_result}")
-    return preflight_validations_result
-
-
-def run_preflight_validations(agentless_gws, target_version):
-    print("----- Preflight validations")
-    print("check the version of the gateway")
-    python_location = run_get_python_location_script(agentless_gws[0])
-
-    preflight_validations_result_json = run_preflight_validations_script(agentless_gws[0], target_version,
-                                                                         python_location)
-
-    preflight_validations_result = json.loads(preflight_validations_result_json)
-    return preflight_validations_result
-
-    # TODO run validations
-    # print("version: 4.11")
-    # print("check the version of the hub")
-    # print("version: 4.11")
-    # print(f"compare the version of the gateway and hub and target version: {args.target_version}")
-    print("check disk space")
-    slp(very_long_sleep_seconds)
-
-
-def run_upgrade_script(agentless_gw, target_version):
-    if run_dummy_upgrade:
-        script_file_name = 'dummy_upgrade_script.sh'
-    else:
-        script_file_name = 'upgrade_v4_10.sh'
-    script_file_path = get_script_file_path(script_file_name)
-    script_contents = read_file_contents(script_file_path)
-
-    args = "1ef8de27-ed95-40ff-8c08-7969fc1b7901 jsonar-4.12.0.10.0.tar.gz us-east-1"
-    script_run_command = build_bash_script_run_command(script_contents, args)
-    # print(f"script_run_command: {script_run_command}")
-
-    script_output = run_remote_script_maybe_with_proxy(agentless_gw, script_contents, script_run_command)
-
-    print(f"Upgrade bash script output: {script_output}")
-    # This relies on the fact that Sonar outputs the string "Upgrade completed"
-    return "Upgrade completed" in script_output
-
-
-def upgrade_gw(agentless_gw, gw_type, target_version):
-    print(f"----- upgrade gw {agentless_gw} of type {gw_type}")
-    print("take backup of the gateway")
-    slp(short_sleep_seconds)
-    print(f"----- upgrade gateway {gw_type}")
-    slp(short_sleep_seconds)
-    print("run upgrade script")
-    result = run_upgrade_script(agentless_gw, target_version)
-    if result:
-        print(f"Upgrading gateway {agentless_gw} was ### successful ###")
-    else:
-        print(f"Upgrading gateway {agentless_gw} ### failed ### ")
-    return result
-
-
-def run_upgrade(agentless_gws, target_version):
-    print("----- upgrade Agentless Gateway DR")
-    return upgrade_gw(agentless_gws[0], "DR", target_version)
-    # print("----- upgrade gw Main")
-    # upgrade_gw("Main", args.target_version)
-
-
-def print_gw(agentless_gws):
-    for item in agentless_gws:
-        # Extract values from the JSON
-        ip = item.get("ip")
-        ssh_user = item.get("ssh_user")
-        ssh_key = item.get("ssh_private_key_file_path")
-        proxy = item.get("proxy")
-
-        # Use the extracted values
-        print(f"IP: {ip}")
-        print(f"SSH User: {ssh_user}")
-        print(f"SSH Key: {ssh_key}")
-        print(f"Proxy: {proxy}")
-        print("---")
-
-
-def run_postflight_validations():
-    print("----- Postflight validations")
-    # TODO
-    print("check the version of the gateway")
-    print("version: 4.12")
-
-
-def str_to_bool(arg):
-    # Convert string "true" or "false" to Python boolean value
-    return arg.lower() == "true"
+# Main functions
 
 
 def main():
@@ -238,6 +127,121 @@ def main():
         run_postflight_validations()
 
     print("********** End ************")
+
+
+def run_preflight_validations(agentless_gws, target_version):
+    print("----- Preflight validations")
+    print("check the version of the gateway")
+    python_location = run_get_python_location_script(agentless_gws[0])
+
+    preflight_validations_result_json = run_preflight_validations_script(agentless_gws[0], target_version,
+                                                                         python_location)
+
+    preflight_validations_result = json.loads(preflight_validations_result_json)
+    return preflight_validations_result
+
+    # TODO run validations
+    # print("version: 4.11")
+    # print("check the version of the hub")
+    # print("version: 4.11")
+    # print(f"compare the version of the gateway and hub and target version: {args.target_version}")
+    print("check disk space")
+    slp(very_long_sleep_seconds)
+
+
+def run_get_python_location_script(agentless_gw):
+    script_file_path = get_script_file_path("get_python_location.sh")
+    script_contents = read_file_contents(script_file_path)
+    script_run_command = build_bash_script_run_command(script_contents)
+
+    script_output = run_remote_script_maybe_with_proxy(agentless_gw, script_contents, script_run_command)
+
+    print(f"'Get python location' bash script output: {script_output}")
+    python_location = extract_python_location(script_output)
+    print(f"Python location in Agentless Gateway {agentless_gw.get('ip')} is {python_location}")
+    return python_location
+
+
+def extract_python_location(script_output):
+    pattern = r'Python location: (\S+)'
+    match = re.search(pattern, script_output)
+
+    if match:
+        return match.group(1)
+    else:
+        raise Exception("Pattern 'Python location: ...' not found in 'Get python location' script output")
+
+
+def run_preflight_validations_script(agentless_gw, target_version, python_location):
+    script_file_path = get_script_file_path("run_preflight_validations.py")
+    script_contents = read_file_contents(script_file_path)
+    script_run_command = build_python_script_run_command(script_contents, target_version, python_location)
+    # print(f"script_run_command: {script_run_command}")
+
+    script_output = run_remote_script_maybe_with_proxy(agentless_gw, script_contents, script_run_command)
+    print(f"'Run preflight validations' python script output: {script_output}")
+    preflight_validations_result = extract_preflight_validations_result(script_output)
+    print(f"Preflight validation results in Agentless Gateway {agentless_gw.get('ip')} are {preflight_validations_result}")
+    return preflight_validations_result
+
+
+def extract_preflight_validations_result(script_output):
+    pattern = r'Preflight validations result: ({.+})'
+    match = re.search(pattern, script_output)
+
+    if match:
+        return match.group(1)
+    else:
+        raise Exception("Pattern 'Preflight validations result: ...' not found in 'Run preflight validations' "
+                        "script output")
+
+
+def run_upgrade(agentless_gws, target_version):
+    print("----- upgrade Agentless Gateway DR")
+    return upgrade_gw(agentless_gws[0], "DR", target_version)
+    # print("----- upgrade gw Main")
+    # upgrade_gw("Main", args.target_version)
+
+
+def upgrade_gw(agentless_gw, gw_type, target_version):
+    print(f"----- upgrade gw {agentless_gw} of type {gw_type}")
+    print("take backup of the gateway")
+    slp(short_sleep_seconds)
+    print(f"----- upgrade gateway {gw_type}")
+    slp(short_sleep_seconds)
+    print("run upgrade script")
+    result = run_upgrade_script(agentless_gw, target_version)
+    if result:
+        print(f"Upgrading gateway {agentless_gw} was ### successful ###")
+    else:
+        print(f"Upgrading gateway {agentless_gw} ### failed ### ")
+    return result
+
+
+def run_upgrade_script(agentless_gw, target_version):
+    if run_dummy_upgrade:
+        script_file_name = 'dummy_upgrade_script.sh'
+    else:
+        script_file_name = 'upgrade_v4_10.sh'
+    script_file_path = get_script_file_path(script_file_name)
+    script_contents = read_file_contents(script_file_path)
+
+    args = "1ef8de27-ed95-40ff-8c08-7969fc1b7901 jsonar-4.12.0.10.0.tar.gz us-east-1"
+    script_run_command = build_bash_script_run_command(script_contents, args)
+    # print(f"script_run_command: {script_run_command}")
+
+    script_output = run_remote_script_maybe_with_proxy(agentless_gw, script_contents, script_run_command)
+
+    print(f"Upgrade bash script output: {script_output}")
+    # This relies on the fact that Sonar outputs the string "Upgrade completed"
+    return "Upgrade completed" in script_output
+
+
+def run_postflight_validations():
+    print("----- Postflight validations")
+    # TODO
+    print("check the version of the gateway")
+    print("version: 4.12")
 
 
 if __name__ == "__main__":
