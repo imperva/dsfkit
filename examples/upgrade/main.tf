3e7d792e576f54a4f0c7f23bd3fb989f41e360a9
linda.nasredin <linda.nasredin@imperva.com>
2023-08-15 21:10:58 +0300

    Sonar upgrade POC - Updated example's main file

--- examples/upgrade/main.tf
+++ examples/upgrade/main.tf
@@ -1,48 +1,46 @@
-
-
 module "sonar_upgrader"{
   source = "./modules/sonar_upgrader_python"
   agentless_gws = [
       {
-        "ip" = "10.0.1.231"
+        "ip" = "10.0.1.1"
         "ssh_user" = "ec2-user"
-        "ssh_private_key_file_path" = "/Users/linda.nasredin/cnc_workspace/dsfkit/examples/poc/sonar_basic_deployment/ssh_keys/dsf_ssh_key-default"
+        "ssh_private_key_file_path" = "/home/ssh_key2.pem"
         "proxy" = {
-          "ip" = "52.8.79.39"
+          "ip" = "52.8.8.8"
           "ssh_user" = "ec2-user"
-          "ssh_private_key_file_path" = "/Users/linda.nasredin/cnc_workspace/dsfkit/examples/poc/sonar_basic_deployment/ssh_keys/dsf_ssh_key-default"
+          "ssh_private_key_file_path" = "/home/ssh_key2.pem"
         }
       },
       {
-        "ip" = "10.0.1.223"
+        "ip" = "10.0.1.2"
         "ssh_user" = "ec2-user"
-        "ssh_private_key_file_path" = "/Users/linda.nasredin/cnc_workspace/dsfkit/examples/poc/sonar_basic_deployment/ssh_keys/dsf_ssh_key-default"
+        "ssh_private_key_file_path" = "/home/ssh_key2.pem"
         "proxy" = {
-          "ip" = "52.8.79.39"
+          "ip" = "52.8.8.8"
           "ssh_user" = "ec2-user"
-          "ssh_private_key_file_path" = "/Users/linda.nasredin/cnc_workspace/dsfkit/examples/poc/sonar_basic_deployment/ssh_keys/dsf_ssh_key-default"
+          "ssh_private_key_file_path" = "/home/ssh_key2.pem"
         }
       },
       {
-        "ip" = "10.0.1.109"
+        "ip" = "10.0.1.3"
         "ssh_user" = "ec2-user"
-        "ssh_private_key_file_path" = "/Users/linda.nasredin/cnc_workspace/dsfkit/examples/poc/sonar_basic_deployment/ssh_keys/dsf_ssh_key-default"
+        "ssh_private_key_file_path" = "/home/ssh_key2.pem"
         "proxy" = {
-          "ip" = "52.8.79.39"
+          "ip" = "52.8.8.8"
           "ssh_user" = "ec2-user"
-          "ssh_private_key_file_path" = "/Users/linda.nasredin/cnc_workspace/dsfkit/examples/poc/sonar_basic_deployment/ssh_keys/dsf_ssh_key-default"
+          "ssh_private_key_file_path" = "/home/ssh_key2.pem"
         }
       }
   ]
   dsf_hubs = [
     {
-      "ip" = "52.8.79.39"
+      "ip" = "52.8.8.8"
       "ssh_user" = "ec2-user"
-      "ssh_private_key_file_path" = "/Users/linda.nasredin/cnc_workspace/dsfkit/examples/poc/sonar_basic_deployment/ssh_keys/dsf_ssh_key-default"
+      "ssh_private_key_file_path" = "/home/ssh_key2.pem"
     }
   ]
 
-  target_version = "4.13.0.10.0-rc5"
+  target_version = "4.12.0.10.0"
   # options
   run_preflight_validations = true
   run_postflight_validations = true
