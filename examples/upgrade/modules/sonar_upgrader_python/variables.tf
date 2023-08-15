b7b6fa59c64c2d93bf0cf140568132e568c5f868
linda.nasredin <linda.nasredin@imperva.com>
2023-08-15 21:22:05 +0300

    Sonar upgrade POC - Added descriptions to the module variables

--- examples/upgrade/modules/sonar_upgrader_python/variables.tf
+++ examples/upgrade/modules/sonar_upgrader_python/variables.tf
@@ -11,6 +11,7 @@ variable "agentless_gws" {
   }))
 
   default     = []
+  description = "A list of the Agentless Gateways to upgrade and their details which are required in order to connect to them to perform the upgrade. The proxy is optional."
 }
 
 variable "dsf_hubs" {
@@ -26,29 +27,35 @@ variable "dsf_hubs" {
   }))
 
   default     = []
+  description = "A list of the DSF Hubs to upgrade and their details which are required in order to connect to them to perform the upgrade. The proxy is optional."
 }
 
 variable "target_version" {
   type        = string
   default     = null
+  description = "The Sonar target version to upgrade to. The lowest supported version is 4.10 from the second patch onward."
 }
 
 variable "run_preflight_validations" {
-    type        = bool
-    default     = true
+  type        = bool
+  default     = true
+  description = "Whether to run the preflight validations or skip them"
 }
     
 variable "run_postflight_validations" {
-    type        = bool
-    default     = true
+  type        = bool
+  default     = true
+  description = "Whether to run the postflight validations or skip them"
 }
 
 variable "custom_validations_scripts" {
-    type        = list(string)
-    default     = []
+  type        = list(string)
+  default     = []
+  description = "A list of scripts with custom validations. This variable is not operational in this POC."
 }
 
 variable "run_upgrade" {
   type        = bool
   default     = true
+  description = "Whether to run upgrade or skip it"
 }
