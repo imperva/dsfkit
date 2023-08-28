29c50c99ead8620287236c54eaa9d03505a233a2
segev.elmalech <segev.elmalech@imperva.com>
2023-08-28 16:37:27 +0300

    add tarball_location varible to dsf example [skip actions]

--- examples/poc/dsf_deployment/sonar.tf
+++ examples/poc/dsf_deployment/sonar.tf
@@ -1,6 +1,6 @@
 locals {
   database_cidr      = var.database_cidr != null ? var.database_cidr : local.workstation_cidr_24
-  tarball_location   = module.globals.tarball_location
+  tarball_location   = var.tarball_location != null ? var.tarball_location : module.globals.tarball_location
   agentless_gw_count = var.enable_sonar ? var.agentless_gw_count : 0
 
   hub_public_ip           = var.enable_sonar ? (length(module.hub[0].public_ip) > 0 ? format("%s/32", module.hub[0].public_ip) : null) : null
--- examples/poc/dsf_deployment/variables.tf
+++ examples/poc/dsf_deployment/variables.tf
@@ -198,6 +198,15 @@ variable "sonar_version" {
     error_message = "The sonar_version value must be 4.11 or higher"
   }
 }
+variable "tarball_location" {
+  type = object({
+    s3_bucket = string
+    s3_region = string
+    s3_key    = string
+  })
+  description = "S3 bucket location of the DSF installation software. s3_key is the full path to the tarball file within the bucket, for example, 'prefix/jsonar-x.y.z.w.u.tar.gz'"
+  default     = null
+}
 
 variable "hub_hadr" {
   type        = bool
