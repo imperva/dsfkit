cb37a761c048ed1be8c0b328d35092996b8b4c0e
linda.nasredin <linda.nasredin@imperva.com>
2023-09-06 12:03:15 +0300

    EDSF-372 Detect changes on gradual upgrade Added upgrade_state_service API with partial implementation

--- examples/sonar_upgrade_poc/main.tf
+++ examples/sonar_upgrade_poc/main.tf
@@ -2,48 +2,21 @@ module "sonar_upgrader"{
   source = "./modules/sonar_upgrader_python"
   agentless_gws = [
       {
-        "host" = "10.0.1.1"
+        "host" = "10.0.1.131"
         "ssh_user" = "ec2-user"
-        "ssh_private_key_file_path" = "/home/ssh_key2.pem"
+        "ssh_private_key_file_path" = "/Users/linda.nasredin/cnc_workspace/dsfkit/examples/poc/sonar_hadr_deployment/ssh_keys/dsf_ssh_key-default"
         "proxy" = {
-          "host" = "52.8.8.8"
+          "host" = "13.52.18.235"
           "ssh_user" = "ec2-user"
-          "ssh_private_key_file_path" = "/home/ssh_key2.pem"
-        }
-      },
-      {
-        "host" = "10.0.1.2"
-        "ssh_user" = "ec2-user"
-        "ssh_private_key_file_path" = "/home/ssh_key2.pem"
-        "proxy" = {
-          "ip" = "52.8.8.8"
-          "ssh_user" = "ec2-user"
-          "ssh_private_key_file_path" = "/home/ssh_key2.pem"
-        }
-      },
-      {
-        "host" = "10.0.1.3"
-        "ssh_user" = "ec2-user"
-        "ssh_private_key_file_path" = "/home/ssh_key2.pem"
-        "proxy" = {
-          "ip" = "52.8.8.8"
-          "ssh_user" = "ec2-user"
-          "ssh_private_key_file_path" = "/home/ssh_key2.pem"
+          "ssh_private_key_file_path" = "/Users/linda.nasredin/cnc_workspace/dsfkit/examples/poc/sonar_hadr_deployment/ssh_keys/dsf_ssh_key-default"
         }
       }
-  ]
-  dsf_hubs = [
-    {
-      "host" = "52.8.8.8"
-      "ssh_user" = "ec2-user"
-      "ssh_private_key_file_path" = "/home/ssh_key2.pem"
-    }
   ]
 
   target_version = "4.12.0.10.0"
   # options
   run_preflight_validations = true
-  run_upgrade = true
-  run_postflight_validations = true
+  run_upgrade = false
+  run_postflight_validations = false
   custom_validations_scripts = ["validation1"]
 }
