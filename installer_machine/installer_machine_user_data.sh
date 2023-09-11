3cd421674b873edd657236d4547d9599b4904201
linda.nasredin <linda.nasredin@imperva.com>
2023-09-11 16:51:31 +0300

    Updated TF version in installer machine to 1.5.7

--- installer_machine/installer_machine_user_data.sh
+++ installer_machine/installer_machine_user_data.sh
@@ -8,7 +8,7 @@ yum -y install git
 
 yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-yum -y install terraform-1.4.5
+yum -y install terraform-1.5.7
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 yum -y install unzip
