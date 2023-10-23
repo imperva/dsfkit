888d921df30957ff9f6ef807c274435a14d04bfd
segev.elmalech <segev.elmalech@imperva.com>
2023-10-23 17:27:18 +0300

    change to remote-exec [skip actions]

--- modules/aws/hub/dra_assocoation.tf
+++ modules/aws/hub/dra_assocoation.tf
@@ -1,18 +1,30 @@
 locals {
-  host                      = var.use_public_ip ? module.hub_instance.public_ip : module.hub_instance.private_ip
   # we are using one password for all services and we have one DRA only
   admin_password            = length(var.dra_details) == 0 ? "" : urlencode(var.dra_details.password)
   archiver_password         = length(var.dra_details) == 0 ? "" : urlencode(var.dra_details.archiver_password)
-  dra_association_commands  = <<-EOT
-    curl -k --max-time 10000 -X POST -G 'https://${local.host}:8443/register-to-dra' -d adminIpOrHostname=${var.dra_details.address} -d adminRegistrationPassword=${local.admin_password} -d adminReportingServer=true -d analyticsArchiveUsername=${var.dra_details.username} -d analyticsArchivePassword=${local.archiver_password} -d resumeDraJobs=true --header "Authorization: Bearer ${module.hub_instance.access_tokens["dam-to-hub"].token}"
-    EOT
+  dra_association_commands  = <<-EOF
+    curl -k --max-time 10000 -X POST -G 'https://127.0.0.1:8443/register-to-dra' -d adminIpOrHostname=${var.dra_details.address} -d adminRegistrationPassword=${local.admin_password} -d adminReportingServer=true -d analyticsArchiveUsername=${var.dra_details.username} -d analyticsArchivePassword=${local.archiver_password} -d resumeDraJobs=true --header "Authorization: Bearer ${module.hub_instance.access_tokens["dam-to-hub"].token}"
+    EOF
 }
 
 resource "null_resource" "dra_association" {
   count = length(local.dra_association_commands) > 0 ? 1 : 0
 
-  provisioner "local-exec" {
-    command = local.dra_association_commands
+  connection {
+    type        = "ssh"
+    user        = module.hub_instance.ssh_user
+    private_key = file(var.ssh_key_pair.ssh_private_key_file_path)
+    host        = var.use_public_ip ? module.hub_instance.public_ip : module.hub_instance.private_ip
+
+    bastion_host        = local.bastion_host
+    bastion_private_key = local.bastion_private_key
+    bastion_user        = local.bastion_user
+
+    script_path = local.script_path
+  }
+
+  provisioner "remote-exec" {
+    inline = concat([local.dra_association_commands])
   }
   depends_on = [
     module.hub_instance.ready
