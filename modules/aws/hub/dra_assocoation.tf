0fff0f5fb6757b872e9e2989def7818aaf2eecd7
segev.elmalech <segev.elmalech@imperva.com>
2023-10-24 14:13:29 +0300

    fix dra_association_commands when var.dra_details == null

--- modules/aws/hub/dra_assocoation.tf
+++ modules/aws/hub/dra_assocoation.tf
@@ -4,7 +4,7 @@ locals {
   archiver_password         = var.dra_details == null ? "" : urlencode(var.dra_details.archiver_password)
   admin_username            = var.dra_details == null ? "" : var.dra_details.username
   admin_address             = var.dra_details == null ? "" : var.dra_details.address
-  dra_association_commands  = <<-EOF
+  dra_association_commands  = var.dra_details == null ? "" : <<-EOF
     curl -k --max-time 10000 -X POST -G 'https://127.0.0.1:8443/register-to-dra' -d adminIpOrHostname=${local.admin_address} -d adminRegistrationPassword=${local.admin_password} -d adminReportingServer=true -d analyticsArchiveUsername=${local.admin_username} -d analyticsArchivePassword=${local.archiver_password} -d resumeDraJobs=true --header "Authorization: Bearer ${module.hub_instance.access_tokens["dam-to-hub"].token}"
     EOF
 }
