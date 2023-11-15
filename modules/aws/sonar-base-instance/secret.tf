f7dc2c31f74759f586d0e91bc7822b251b40f79c
linda.nasredin <linda.nasredin@imperva.com>
2023-11-15 17:21:58 +0200

    Added missing "SSH" to keys descriptions

--- modules/aws/sonar-base-instance/secret.tf
+++ modules/aws/sonar-base-instance/secret.tf
@@ -51,7 +51,7 @@ locals {
 resource "aws_secretsmanager_secret" "sonarw_private_key_secret" {
   count       = local.should_create_sonarw_private_key_in_secrets_manager == true ? 1 : 0
   name_prefix = "${var.name}-sonarw-private-key"
-  description = "Imperva DSF node sonarw user private key - used for remote Agentless Gateway federation, HADR, etc."
+  description = "Imperva DSF node sonarw user SSH private key - used for remote Agentless Gateway federation, HADR, etc."
   tags        = var.tags
 }
 
