c57058e6bda06b178c7ca99ca66ccf849ff1b307
Eytan Naim <eytan.naim@imperva.com>
2023-11-20 11:18:05 +0200

    wip

--- modules/aws/mx/hub.tf
+++ modules/aws/mx/hub.tf
@@ -7,12 +7,12 @@ locals {
   hub_action_set = local.dam_version != "14.7" ? local._hub_action_set : local._hub_action_set147
   hub_action_set_action = local.dam_version != "14.7" ? local._hub_action_set_action : local._hub_action_set_action147
 
+  # Archiving action set is created differently on 14.7
   dam_version_major = split(".", var.dam_version)[0]
   dam_version_minor = split(".", var.dam_version)[1]
   dam_version = "${local.dam_version_major}.${local.dam_version_minor}"
-  # Archiving action set is created differently on 14.7
 
-  action_set_item = var.hub_details == [] ? null : local.dam_version != "14.7" ? [{
+  action_set_item = var.hub_details == null ? [] : local.dam_version != "14.7" ? [{
     name     = "send_to_hub_action_set" # https://docs.imperva.com/bundle/v14.11-database-activity-monitoring-user-guide/page/78508.htm
     method   = "PUT"
     url_path = "SecureSphere/api/v1/conf/actionSets/${local.hub_action_set}/${local.hub_action_set_action}"
@@ -40,7 +40,7 @@ locals {
     )
     }]
 
-  hub_configuration = concat(local.action_set_item,
+  hub_configuration = var.hub_details == null ? [] : concat(local.action_set_item,
     var.large_scale_mode == true ? [] : [{
       name     = "archive_default_audit_policy_to_hub" # https://docs.imperva.com/bundle/v14.11-database-activity-monitoring-user-guide/page/78508.htm
       method   = "PUT"
