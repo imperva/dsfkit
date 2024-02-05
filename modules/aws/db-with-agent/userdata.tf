d510bc0be1f73b24d4697221f0ac0efadda0b74b
linda.nasredin <linda.nasredin@imperva.com>
2024-02-05 14:05:24 +0200

    EDSF-535 Add prefix to tarball/agent s3 bucket

--- modules/aws/db-with-agent/userdata.tf
+++ modules/aws/db-with-agent/userdata.tf
@@ -17,7 +17,7 @@ locals {
       export PATH=$PATH:/usr/local/bin:/usr/local/bin
       echo "Downloading agent:"
       INSTALLATION_FILE=${local.s3_object}
-      aws s3 cp s3://${local.s3_bucket}/${local.s3_key} . --region ${local.s3_region}
+      aws s3 cp s3://${var.binaries_location.s3_bucket}/${local.s3_key} . --region ${var.binaries_location.s3_region}
       chmod +x ./"$INSTALLATION_FILE"
       echo "Installing agent:"
       ./"$INSTALLATION_FILE" -n -d ${local.agent_installation_dir}
