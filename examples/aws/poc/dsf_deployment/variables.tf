b83bcf39fbe19224016bf0a1c1d8bd777558b6b9
jagdeep-sonar <jagdeepkaur.saini@imperva.com>
2024-11-27 14:43:16 -0800

    updating apps directory size and test instance type

--- examples/aws/poc/dsf_deployment/variables.tf
+++ examples/aws/poc/dsf_deployment/variables.tf
@@ -246,7 +246,7 @@ variable "agentless_gw_hadr" {
 
 variable "hub_instance_type" {
   type        = string
-  default     = "r6i.xlarge"
+  default     = "r6i.2xlarge"
   description = "Ec2 instance type for the DSF Hub"
 }
 
