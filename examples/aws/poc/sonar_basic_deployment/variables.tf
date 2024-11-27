dcecce7bb8c953ab1a8b41bb8d4b9e24bb5d450c
jagdeep-sonar <jagdeepkaur.saini@imperva.com>
2024-11-27 14:43:16 -0800

    updating apps directory size and test instance type

--- examples/aws/poc/sonar_basic_deployment/variables.tf
+++ examples/aws/poc/sonar_basic_deployment/variables.tf
@@ -94,7 +94,7 @@ variable "subnet_ids" {
 
 variable "hub_instance_type" {
   type        = string
-  default     = "r6i.xlarge"
+  default     = "r6i.2xlarge"
   description = "Ec2 instance type for the DSF Hub"
 }
 
